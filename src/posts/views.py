621e95640487d04c5bac1d16acc85ebb28a35273
Jared E Jackson <jaredjxn15@gmail.com>
2018-11-14 20:57:07 -0500

    Added post route handlers and template directory

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def post_create(request):
+    context = {
+        "title": "Create"
+    }
+    return HttpResponse("<h1>Create</h1>")
 
-def posts_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_detail(request):
+    context = {
+        "title": "Detail"
+    }
+    return HttpResponse("<h1>Detail</h1>")
+
+def post_list(request):
+    context = {
+        "title": "List"
+    }
+    return render(request, "index.html", context)
+    #return HttpResponse("<h1>List</h1>")
+
+def post_update(request):
+    context = {
+        "title": "Update"
+    }
+    return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+    context = {
+        "title": "Delete"
+    }
+    return HttpResponse("<h1>Delete</h1>")
